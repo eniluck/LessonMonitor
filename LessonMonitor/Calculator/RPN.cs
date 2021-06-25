aae71294d721cafff32d0aa1f99730936569ece2
Федерягин Андрей <eniluck@mail.ru>
2021-06-25 17:13:13 +0500

    add project

---
+++ LessonMonitor/Calculator/RPN.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    // Обратная польская запись Reverse Polish Notation
+    public class RPN
+    {
+        // Вычисление входной строки
+        public decimal Calculate(string input)
+        {
+            // Преобразовываем строку в постфиксную запись
+            Stack<decimal> RPN = GetRPNfromString(input);
+
+            //Решаем полученное выражение
+            decimal result = Counting(output); 
+            return result; 
+        }
+
+        private Stack<decimal> GetRPNfromString(string input)
+        {
+            Stack<decimal> result = new Stack<decimal>();
+            Stack<char> operStack = new Stack<char>();      //Стек для хранения операторов
+
+
+            return null;
+        }
+    }
+}
